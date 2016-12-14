e587349fa0db8d7ebfddfb7c2e8308edd13197b5
Joshua Plicque <plicjo@gmail.com>
2016-12-13 21:20:11 -0500

    Code cleanup

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe '#less_than_three_levels_deep?' do
-    let(:user) { FactoryGirl.create(:user) }
+    let(:user)    { FactoryGirl.create(:user) }
     let(:stylist) { FactoryGirl.create(:stylist, user: user) }
-    let(:work) { FactoryGirl.create(:work, stylist: stylist) }
+    let(:work)    { FactoryGirl.create(:work, stylist: stylist) }
 
     context '1 level deep' do
       it 'returns true' do
@@ -14,7 +14,7 @@ RSpec.describe Comment, type: :model do
     end
 
     context '3 levels deep' do
-      let!(:first_level_comment) { work.comments.create(body: 'first_level', user: user) }
+      let!(:first_level_comment)  { work.comments.create(body: 'first_level', user: user) }
       let!(:second_level_comment) { first_level_comment.comments.create(body: 'second_level', user: user) }
 
       it 'returns true' do
@@ -24,9 +24,9 @@ RSpec.describe Comment, type: :model do
     end
 
     context '4 levels deep' do
-      let!(:first_level_comment) { work.comments.create(body: 'first_level', user: user) }
+      let!(:first_level_comment)  { work.comments.create(body: 'first_level', user: user) }
       let!(:second_level_comment) { first_level_comment.comments.create(body: 'second_level', user: user) }
-      let!(:third_level_comment) { second_level_comment.comments.create(body: 'third_level', user: user) }
+      let!(:third_level_comment)  { second_level_comment.comments.create(body: 'third_level', user: user) }
 
       it 'returns false' do
         fourth_level_comment = third_level_comment.comments.create(body: 'fourth_level', user: user)
